dceddd5d96c8fae582a540eb849fb3f510263a24
cris-ctrl <cristianwalter21@gmail.com>
2024-10-10 15:21:58 -0300

    following ursina tutorial

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from ursina import *
 app = Ursina()
 
+def update():
+    if held_keys['a']:
+        test_square.x -= 4 * time.dt
+
+test_square = Entity(model = 'cube', color =  color.red)
+
+
 app.run()
